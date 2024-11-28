68bd943f17dfdbdb6ba94ab146e62af0eda4bdc8
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-11-28 15:35:07 +0100

    Update ViewManager.java

--- src/main/java/com/openwar/charpy/openwarlauncher/utils/ViewManager.java
+++ src/main/java/com/openwar/charpy/openwarlauncher/utils/ViewManager.java
@@ -1,5 +1,8 @@
 package com.openwar.charpy.openwarlauncher.utils;
 
+import com.openwar.charpy.openwarlauncher.controller.AuthPageController;
+import com.openwar.charpy.openwarlauncher.controller.MainPageController;
+import com.openwar.charpy.openwarlauncher.controller.SettingsPageController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,17 +19,31 @@ public class ViewManager {
         this.stage = stage;
     }
 
-    public void showPage(String fxmlPath, String title, int width, int height) {
+    public void showPage(String fxmlPath, String title, int width, int height, PlayerProfile pf) {
         try {
             StackPane root = new StackPane();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/openwar/charpy/openwarlauncher/" + fxmlPath));
             Parent page = loader.load();
+            Object controller = loader.getController();
+            System.out.println("controller "+controller);
+            if (controller instanceof AuthPageController) {
+                System.out.println("AUTH View Manager for "+ controller);
+                ((AuthPageController) controller).setViewManager(this);
+            } else if (controller instanceof MainPageController) {
+                System.out.println("MAIN View Manager for "+ controller);
+                ((MainPageController) controller).setViewManager(this);
+                ((MainPageController) controller).setPlayerProfile(pf);
+            } else if (controller instanceof SettingsPageController) {
+                System.out.println("SETTINGS View Manager for "+ controller);
+                ((SettingsPageController) controller).setViewManager(this);
+            }
             root.getChildren().add(page);
             stage.setResizable(false);
             stage.setScene(new Scene(root, width, height));
             stage.getIcons().add(new Image("https://openwar.fr/public/images/op.png"));
             stage.setTitle(title);
             stage.show();
+
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Erreur lors du chargement de la page : " + fxmlPath);

568cf120a889037298bfa8f6642f3a7d93015b28
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-11-28 15:35:01 +0100

    Create SettingsPageController.java

---
+++ src/main/java/com/openwar/charpy/openwarlauncher/controller/SettingsPageController.java
@@ -0,0 +1,18 @@
+package com.openwar.charpy.openwarlauncher.controller;
+
+import com.openwar.charpy.openwarlauncher.utils.ViewManager;
+import javafx.fxml.FXML;
+
+public class SettingsPageController {
+
+
+    private ViewManager viewManager;
+
+    public void setViewManager(ViewManager viewManager) {
+        this.viewManager = viewManager;
+    }
+    @FXML
+    public void initialize() {
+
+    }
+}

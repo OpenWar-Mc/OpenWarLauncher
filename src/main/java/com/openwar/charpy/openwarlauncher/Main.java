79e6f58094fab98c6410f0780a3b20f61f360f58
Charpy <76815325+Charpyy@users.noreply.github.com>
2024-11-28 15:35:04 +0100

    Update Main.java

--- src/main/java/com/openwar/charpy/openwarlauncher/Main.java
+++ src/main/java/com/openwar/charpy/openwarlauncher/Main.java
@@ -3,19 +3,8 @@ package com.openwar.charpy.openwarlauncher;
 import com.openwar.charpy.openwarlauncher.controller.AuthPageController;
 import com.openwar.charpy.openwarlauncher.utils.ViewManager;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 public class Main extends Application {
@@ -24,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         viewManager = new ViewManager(stage);
-        viewManager.showPage("AuthPage.fxml", "OpenWar - Authentication", 400, 640);
+        viewManager.showPage("AuthPage.fxml", "OpenWar - Authentication", 400, 640, null);
     }
 
     public static void main(String[] args) {
